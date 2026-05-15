3118a47f39ef35cc84d882216b2a50a3f1130443
kimkw4784 <kimkw4784@gmail.com>
2026-05-15 13:01:21 +0900

    feat: implement Header component with branding assets and project documentation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import './Header.css';
+import logo-image from "/logo.png";
 
 const Header = ({ onMenuClick }) => {
   return (
     <header className="header">
       <div className="header-logo" role="img" aria-label="화성시립도서관 로고">
-        <img 
-          src="/logo.png" 
+        <img
+          src={logo}
           alt="" 
-          className="logo-image" 
+          className="logo-image"
         />
         <h1 className="site-name">화성시립도서관</h1>
       </div>
       <button className="menu-button" aria-label="메뉴 열기" onClick={onMenuClick}>
         <svg width="32" height="24" viewBox="0 0 33 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 2H33M0 12H33M0 22H33" stroke="var(--color-black)" strokeWidth="3"/>
+          <path d="M0 2H33M0 12H33M0 22H33" stroke="var(--color-black)" strokeWidth="3" />
         </svg>
       </button>
     </header>
